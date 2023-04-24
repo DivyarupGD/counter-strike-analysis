3f695aa499012cb210ddf847d0748c25d7dcf695
Divyarup Ghosh Dastidar <dgdastidar1996@gmail.com>
2023-04-24 23:13:41 +0530

    Added sqoop commands (#3)

--- sql/create_and_load_tables.sql
+++ sql/create_and_load_tables.sql
@@ -1,4 +1,4 @@
-/* Create Players Tables */
+/****************************************** Creating Tables *********************************************/
 
 CREATE TABLE players(name VARCHAR(30), country VARCHAR(30), id INT PRIMARY KEY);
 CREATE TABLE teams(name VARCHAR(30), country VARCHAR(30), id INT PRIMARY KEY);
@@ -8,8 +8,15 @@ CREATE TABLE event_ids(type VARCHAR(30),name VARCHAR(150),match_date VARCHAR(15)
 CREATE TABLE match_ids(id INT PRIMARY KEY, title VARCHAR(150));
 CREATE TABLE match_events(match_title VARCHAR(150), event_id INT);
 CREATE TABLE match_results(match_date VARCHAR(15), map VARCHAR(10), team1_id INT, team1_start_side VARCHAR(3), team1_score INT, team1_h1 INT, team1_h2 INT, team1_o INT, team2_id INT, team2_start_side VARCHAR(3), team2_score INT, team2_h1 INT, team2_h2 INT, team2_o INT, match_id VARCHAR(150));
+/* Tables with no PK:
+player_stats
+event_ids
+match_events
+match_results
+*/
 
-/* Loading Data */
+
+/****************************************** Loading Data *********************************************/
 
 LOAD DATA INFILE "/home/cloudera/Desktop/shared/data/players.csv" INTO TABLE players COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n';
 LOAD DATA INFILE "/home/cloudera/Desktop/shared/data/teams.csv" INTO TABLE teams COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n';
@@ -20,9 +27,12 @@ LOAD DATA INFILE "/home/cloudera/Desktop/shared/data/matchIDs.csv" INTO TABLE ma
 LOAD DATA INFILE "/home/cloudera/Desktop/shared/data/joinMatchEvent.csv" INTO TABLE match_events COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n';
 LOAD DATA INFILE "/home/cloudera/Desktop/shared/data/matchResults.csv" INTO TABLE match_results COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
-/* Tables with no PK:
-player_stats
-event_ids
-match_events
-match_results
-*/
+/****************************************** Verifying load is successful *********************************************/
+SELECT * FROM players LIMIT 10;
+SELECT * FROM teams LIMIT 10;
+SELECT * FROM player_stats LIMIT 10;
+SELECT * FROM match_lineups LIMIT 10;
+SELECT * FROM event_ids LIMIT 10;
+SELECT * FROM match_ids LIMIT 10;
+SELECT * FROM match_events LIMIT 10;
+SELECT * FROM match_results LIMIT 10;
